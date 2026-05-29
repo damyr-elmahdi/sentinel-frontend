8e1b2b7c46e2f3857086c303e477c473db6fe481
mahdi damyr <mahdidamyr@gmail.com>
2026-05-29 22:14:38 +0100

    fixing the navBar

--- src/app/shared/navbar/navbar.ts
+++ src/app/shared/navbar/navbar.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, HostListener, Input } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
@@ -6,10 +6,21 @@ import { CommonModule } from '@angular/common';
   selector: 'app-navbar',
   standalone: true,
   imports: [RouterLink, RouterLinkActive, CommonModule],
-  templateUrl: './navbar.html'
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.css',
+  host: { style: 'display: contents' }
 })
 export class NavbarComponent {
   @Input() transparent = false;
   menuOpen = false;
-  toggleMenu() { this.menuOpen = !this.menuOpen; }
+  scrolled = false;
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  @HostListener('window:scroll')
+  onScroll() {
+    this.scrolled = window.scrollY > 8;
+  }
 }
